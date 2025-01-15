d0c7483373c817d478694d18d9e6b36ccf74f492
Israel Fernandez <israelfernandez96@gmail.com>
2025-01-15 11:05:00 -0800

    + Added root layout with Google Fonts and custom IF favicon

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import '../app.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="" />
+	<link
+		rel="preload"
+		as="style"
+		href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;600&display=swap"
+	/>
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@400;600&display=swap"
+	/>
+</svelte:head>
+
+{@render children()}
